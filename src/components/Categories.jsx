96ef7b70d80a78d692ed851e8a46e5dfb39714d1
GulievCorp <guliev.anar1998@gmail.com>
2021-09-10 21:38:19 +0300

    rework categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,6 +1,5 @@
-import { render } from '@testing-library/react';
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch} from 'react-redux';
 import { setCategory } from '../actions/fillters';
 
 
